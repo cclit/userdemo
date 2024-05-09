0a7ee9bd5abd420ccabe05e51d8bd8794f61705c
Galen Lin <galencml@gmail.com>
2024-05-09 16:08:16 +0800

    Galen 1) project initiated

---
+++ src/main/java/com/cclit/userdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cclit.userdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserdemoApplication.class);
+	}
+
+}
---
+++ src/test/java/com/cclit/userdemo/UserdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cclit.userdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
